7d9de46e40cfbf1d6cd044b705fb2ce00e7ccbbf
淇右 <zhendong.gzd@alibaba-inc.com>
2018-01-08 22:16:30 +0800

    chore: setup

--- scripts/link.js
+++ scripts/link.js
@@ -8,8 +8,8 @@ const PACKAGES_DIR = path.resolve(__dirname, '../packages');
 
 function getPackages() {
   return fs.readdirSync(PACKAGES_DIR)
-    .map(file => path.resolve(PACKAGES_DIR, file))
-    .filter(f => fs.lstatSync(path.resolve(f)).isDirectory());
+    .map(f => path.resolve(PACKAGES_DIR, f))
+    .filter(f => fs.lstatSync(f).isDirectory());
 }
 
 const linkSkipPackages = [

1a3e8c0aed5e38076001e6086a579e4322e3c4a2
松鹤 <songhe.zl@alibaba-inc.com>
2018-01-17 18:06:44 +0800

    fix: clipboard

--- packages/rap-sdk/src/clipboard.js
+++ packages/rap-sdk/src/clipboard.js
@@ -1,17 +1,18 @@
 import { requireModule } from './rap';
 import { log } from './util';
 
+let _clipboard = requireModule('clipboard');
 let clipboard = {
   setString: function(str) {
     if (typeof str === 'string') {
-      clipboard.setString(str);
+      _clipboard.setString(str);
     } else {
       log.error('arguments type error');
     }
   },
 
   getString: function(callback) {
-    clipboard.getString((res) => {
+    _clipboard.getString((res) => {
       // let info = {}
       callback && callback(res);
     });

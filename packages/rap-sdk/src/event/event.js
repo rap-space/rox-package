922f9d45019c2de2e79e6735e52d263bda4ae10d
qoder <qoder.qc@alibaba-inc.com>
2018-01-23 12:10:52 +0800

    feat: update sdk event module

--- packages/rap-sdk/src/event/event.js
+++ packages/rap-sdk/src/event/event.js
@@ -7,6 +7,7 @@ let onPromises = {
   // page.balabala: Promise,
   // app.haha: Promise,
 };
+
 const Event = {
   on: function(eventName, callback) {
     // back
@@ -14,64 +15,54 @@ const Event = {
     // App.back
     // Global.didBecomine;
     let event = buildEventCenterByName(eventName);
-    // if (!onPromises[event.value]) {
-    //   onPromises[event.value] = RapBridge.call({
-    //     className: event.center,
-    //     methodName: 'addListener',
-    //     param: {
-    //       eventName: event.name
-    //     }
-    //   }, function(info) {
-    //     // 这里预处理一些信息
-    //     callback &&　callback(info);
-    //   });
-    // }
+    if (!onPromises[event.value]) {
+      onPromises[event.value] = RapBridge.call({
+        className: event.center,
+        methodName: 'addListener',
+        param: {
+          eventName: event.name
+        }
+      }, function(event) {
+        event.value = event.scope.toLowerCase() + '.' + event.name;
+        eventEmitter.emit(event.value, event.info);
+      });
+    }
 
-    // let promise = onPromises[event.value];
-    // promise.catch(error => {
-    //   delete onPromises[event.value];
-    // });
-    // return promise
-    //   .then(() => {
-    //     eventEmitter.on(event.value, callback);
-    //   });
-
-    return RapBridge.call({
-      className: event.center,
-      methodName: 'addListener',
-      param: {
-        eventName: event.name
-      }
-    }, function(info) {
-      // 这里预处理一些信息
-      callback &&　callback(info);
+    let promise = onPromises[event.value];
+    promise.catch(error => {
+      delete onPromises[event.value];
     });
+
+    return promise
+      .then(() => {
+        eventEmitter.on(event.value, callback);
+      });
   },
   off: function(eventName, callback) {
     let event = buildEventCenterByName(eventName);
-    // return eventEmitter.off(event.value, callback)
-    //   .then(result => {
-    //     let options = eventEmitter.optionsPool[event.value];
-    //     if (options.length > 0) return result;
-    //     if (!onPromises[event.value]) return result;
-    //     return RapBridge.call({
-    //       className: event.center,
-    //       methodName: 'removeListener',
-    //       param: {
-    //         eventName: event.name
-    //       }
-    //     }).then(result => {
-    //       delete onPromises[event.value];
-    //     });
-    //   });
+    return eventEmitter.off(event.value, callback)
+      .then(result => {
+        let options = eventEmitter.optionsPool[event.value];
+        if (options.length > 0) return result;
+        if (!onPromises[event.value]) return result;
+        return RapBridge.call({
+          className: event.center,
+          methodName: 'removeListener',
+          param: {
+            eventName: event.name
+          }
+        }).then(result => {
+          delete onPromises[event.value];
+        });
+      });
 
-    return RapBridge.call({
-      className: event.center,
-      methodName: 'removeListener',
-      param: {
-        eventName: event.name
-      }
-    });
+    // return RapBridge.call({
+    //   className: event.center,
+    //   methodName: 'removeListener',
+    //   param: {
+    //     eventName: event.name
+    //   }
+    // });
   },
 
   emit: function(eventName, options) {
@@ -86,4 +77,5 @@ const Event = {
     });
   }
 };
+
 export default Event;

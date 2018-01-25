9a7d1e2d66fe4ab4840752508ad90f31f0b5634a
qoder <qoder.qc@alibaba-inc.com>
2018-01-25 19:20:54 +0800

    feat: update sdk aop

--- packages/rap-sdk/src/aop.js
+++ packages/rap-sdk/src/aop.js
@@ -3,8 +3,8 @@
 // 2. weex 降级的 主客容器下
 // 3. H5下 import {report} from '@ali/universal-tracker';
 import Rap from './rap';
-import {isWeex, isWeb} from './env';
-import {defer} from './util';
+import { isWeex, isWeb } from './env';
+import { defer, parseJson } from './util';
 
 const RAP_SUCCESS = 'RAP_SUCCESS';
 const RAP_FAILURE = 'RAP_FAILURE';
@@ -17,8 +17,7 @@ const rCode = /FAIL_BIZ_/;
 
 function report() {};
 
-function reportError(params, retJson) {
-  params = params || {};
+function reportError(params = {}, retJson) {
   if (params.disableTracker) {
     return;
   }
@@ -47,15 +46,113 @@ function reportError(params, retJson) {
 
 function requestByRap(options, successCallback, failureCallback) {
   return Rap
-    .call({className: 'mtop', methodName: 'request', options: options})
+    .call({className: 'mtop', methodName: 'request', options})
     .then(successCallback, failureCallback);
+}
+
+/**
+ *
+ * @param {Object} options
+ * @param {String} options.api  开放平台API Name
+ * @param {String} options.version  开放平台 API Version
+ * @param {Obejct} options.params   开放平台 业务参数
+ * @param {String} options.namespace  开放平台 命名空间
+ */
+function formatOpenApiParams(options) {
+  const API = 'mtop.1688.wireless.openapi.gateway';
+  const data = {};
+
+  data.namespace = options.namespace;
+  data.apiName = options.api;
+  data.apiVersion = `${parseInt(options.v || options.version || '1')}`;
+
+  if (typeof options.params === 'object') {// fix: android bug
+    data.params = JSON.stringify(options.params);
+  }
+
+  const params = {
+    api: API,
+    isOpenApi: true, // 约定参数 再通知Native 透传 appKey与 timestamp
+    v: '1.0',
+    data
+  };
+
+  return params;
+}
+
+/**
+ *
+ * @param {Object} options
+ * @param {String} options.targetUrl  代理 url 地址
+ */
+function formatHttpProxyParams(options) {
+  const API = 'mtop.1688.wireless.isv.httpproxy';
+  const data = {};
+
+  data.targetUrl = options.targetUrl;
+  data.ecode = '1';
+  data.method = options.method || 'GET';
+  data.headers = options.headers;
+  data.body = options.body;
+
+  const params = {
+    api: API,
+    v: '1.0',
+    ecode: '1',
+    data
+  };
+
+  return params;
+}
+
+// 这里有成功与失败的场景
+// 成功-能够调通服务
+// 成功-服务调用成功
+// 成功-服务调用失败
+// 失败
+// 失败-网络异常
+// 失败-网关服务异常
+function formatRetJson(retJson) {
+  try {
+    let res = {};
+    let o = {};
+
+    if (typeof retJson === 'string') {
+      retJson = JSON.parse(retJson);
+    }
+
+    retJson = parseJson(retJson);
+
+    if (RAP_SUCCESS === retJson.code) {
+      res = parseJson(retJson.data);
+
+      const ret = res.ret[0].split(RET_BOUND_SYMBOL);
+      const code = ret[0].toUpperCase();
+
+      if (RET_CODE_SUCCESS === code) {
+        o = res.data;
+      } else {
+        o.success = RET_FALSE;
+        o.errorCode = code.replace(rCode, '');
+        o.errorMessage = ret[1] === RET_MESSAGE_NULL ? '' : ret[1];
+      }
+    } else {
+      o.success = RET_FALSE;
+      o.errorCode = RAP_FAILURE;
+      o.errorMessage = '接口调用异常';
+    }
+
+    return o;
+  } catch (e) {
+    console.error(`ERROR:: ${e}`);
+  }
 }
 
 const AOP = {
   request(options, successCallback, failureCallback) {
     const defered = defer();
     const bizType = '3';
-    // 这里有
+
     const _failureCallback = (retJson) => {
       failureCallback = failureCallback || successCallback;
       failureCallback && failureCallback(formatRetJson(retJson));
@@ -84,7 +181,6 @@ const AOP = {
     const _failureCallback = (retJson) => {
       failureCallback = failureCallback || successCallback;
       failureCallback && failureCallback(formatRetJson(retJson));
-      // reportError(params, retJson);
       defered.reject(formatRetJson(retJson));
     };
 
@@ -100,98 +196,5 @@ const AOP = {
     return defered.promise;
   }
 };
-
-// 这里有成功与失败的场景
-// 成功-能够调通服务
-// 成功-服务调用成功
-// 成功-服务调用失败
-// 失败
-// 失败-网络异常
-// 失败-网关服务异常
-function formatRetJson(retJson) {
-  try {
-    let res = {};
-    let o = {};
-
-    if (typeof retJson === 'string') {
-      retJson = JSON.parse(retJson);
-    }
-
-    if (RAP_SUCCESS === retJson.code) {
-      res = retJson.data;
-
-      const ret = res.ret[0].split(RET_BOUND_SYMBOL);
-      const code = ret[0].toUpperCase();
-
-      if (RET_CODE_SUCCESS === code) {
-        o = res.data;
-      } else {
-        o.success = RET_FALSE;
-        o.errorCode = code.replace(rCode, '');
-        o.errorMessage = ret[1] === RET_MESSAGE_NULL ? '' : ret[1];
-      }
-    } else {
-      o.success = RET_FALSE;
-      o.errorCode = RAP_FAILURE;
-      o.errorMessage = '接口调用异常';
-    }
-
-    return o;
-  } catch (e) {
-    console.error(`ERROR:: ${e}`);
-  }
-}
-/**
- *
- * @param {Object} options
- * @param {String} options.api  开放平台API Name
- * @param {String} options.version  开放平台 API Version
- * @param {Obejct} options.params   开放平台 业务参数
- * @param {String} options.namespace  开放平台 命名空间
- */
-function formatOpenApiParams(options) {
-  let params = {};
-  let data = {};
-  data.namespace = options.namespace;
-  data.apiName = options.api;
-  data.apiVersion = '' + parseInt(options.v || options.version || '1');
-  if (typeof options.params === 'object') {// fix: android bug
-    data.params = JSON.stringify(options.params);
-  }
-  params = {
-    api: 'mtop.1688.wireless.openapi.gateway',
-    isOpenApi: true, // 约定参数 再通知Native 透传 appKey与 timestamp
-    v: '1.0',
-    data: data
-  };
-  return params;
-}
-
-/**
- *
- * @param {Object} options
- * @param {String} options.targetUrl  代理 url 地址
- */
-function formatHttpProxyParams(options) {
-  let params = {};
-  let data = {};
-
-  const API = 'mtop.1688.wireless.isv.httpproxy';
-
-  data.targetUrl = options.targetUrl;
-  data.ecode = '1';
-  data.method = options.method || 'GET';
-  data.headers = options.headers;
-  data.body = options.body;
-
-  params = {
-    api: API,
-    v: '1.0',
-    ecode: '1',
-    data: data
-  };
-
-  return params;
-}
 
 export default AOP;

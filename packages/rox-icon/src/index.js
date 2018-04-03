2b366286807029df3b8f52c0afafc5da5f8aed20
zhendong.gzd <zhendong.gzd@alibaba-inc.com>
2018-04-03 13:34:48 +0800

    feat: expose

--- packages/rox-icon/src/index.js
+++ packages/rox-icon/src/index.js
@@ -1,6 +1,7 @@
-import Icon from './view/icon';
+import Icon, { setIconConfig } from './view/icon';
 import iconConfigs from './view/code';
 
 export const getTypeIconNames = type => Object.keys(iconConfigs[type].codes);
+export const setIconConfig = setIconConfig;
 
 export default Icon;

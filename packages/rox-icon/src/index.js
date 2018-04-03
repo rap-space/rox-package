f836b545a9ef68c85797ef093b58df9d12d79983
zhendong.gzd <zhendong.gzd@alibaba-inc.com>
2018-04-03 13:41:13 +0800

    fix: type

--- packages/rox-icon/src/index.js
+++ packages/rox-icon/src/index.js
@@ -2,6 +2,6 @@ import Icon, { setIconConfig } from './view/icon';
 import iconConfigs from './view/code';
 
 export const getTypeIconNames = type => Object.keys(iconConfigs[type].codes);
-export const setIconConfig = setIconConfig;
+export const setIconCodeConfig = setIconConfig;
 
 export default Icon;

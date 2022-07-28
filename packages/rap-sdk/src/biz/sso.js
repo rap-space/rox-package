d141e6c161efccb23e8ef911e67591dc82466a5c
lucien.ly <lucien.ly@alibaba-inc.com>
2022-07-28 20:18:58 +0800

    feat: 授权页面去除wh_weex=true参数，降级到web页面避免坑点

--- packages/rap-sdk/src/biz/sso.js
+++ packages/rap-sdk/src/biz/sso.js
@@ -1,7 +1,7 @@
 import navigator from '../navigator';
 import { getAppKey } from '../util';
 
-const authURL = 'https://air.1688.com/apps/alim/open/authorize.html?wh_weex=true&appKey=';
+const authURL = 'https://air.1688.com/apps/alim/open/authorize.html?appKey=';
 const sso = {
   goAuth: function() { // 最好支持事件
     // getAppKey;
